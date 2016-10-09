451e74f793d448f846d24013781d5c958e445c63
ssai123 <sbs.sai@gmail.com>
2016-10-09 03:53:57 -0400

    vibration and dialog box

---
+++ app/src/main/java/com/example/finessefitness/QuitDialog.java
@@ -0,0 +1,37 @@
+package com.example.finessefitness;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+public class QuitDialog extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("Quit?");
+        builder.setMessage("Are you sure you want to quit this workout?");
+        builder.setNegativeButton(android.R.string.yes,
+                new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // user clicks the yes button
+                    }
+
+                });
+        builder.setPositiveButton(android.R.string.no,
+                new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // user clickes the no button
+                    }
+                });
+        Dialog create = builder.create();
+        create.show();
+        return create;
+    }
+}
--- app/src/main/java/com/example/finessefitness/WorkoutActivity.java
+++ app/src/main/java/com/example/finessefitness/WorkoutActivity.java
@@ -1,9 +1,12 @@
 package com.example.finessefitness;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.CountDownTimer;
+import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -17,7 +20,11 @@ public class WorkoutActivity extends AppCompatActivity {
     private TextView seconds;
     private TextView activity;
     static int curInd;
+    private static CountDownTimer timer;
+    private static boolean quit = false;
+    private Bundle bundle;
 
+    // customized countdown timer
      private class ActivityCountDownTimer extends CountDownTimer {
          ActivityCountDownTimer(long millisInFuture, long countDownInterval){
              super(millisInFuture, countDownInterval);
@@ -32,6 +39,9 @@ public class WorkoutActivity extends AppCompatActivity {
         }
          @Override
         public void onFinish() {
+             if (quit) {
+                 return;
+             }
              curInd++;
             startNextTimer();
         }
@@ -42,30 +52,51 @@ public class WorkoutActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_workout);
 
-        System.out.println(StartWorkoutActivity.workoutExercises);
-        System.out.println(StartWorkoutActivity.exerciseSeconds);
+        this.bundle = savedInstanceState;
 
         seconds = (TextView) findViewById(R.id.seconds);
         activity = (TextView) findViewById(R.id.activity);
 
+        // reset
         curInd = 0;
+        quit = false;
 
         activity.setText(StartWorkoutActivity.workoutExercises.get(0));
         new ActivityCountDownTimer(StartWorkoutActivity.exerciseSeconds.get(0) * 1000, 1000).start();
 
         ViewGroup layout = (ViewGroup) findViewById(R.id.activity_workout);
 
-        // back button
-        getSupportActionBar().setHomeButtonEnabled(true);
+        // disable back arrow
+        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+
+
     }
 
     private void startNextTimer() {
+        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
         if (curInd < StartWorkoutActivity.workoutExercises.size()) {
+            System.out.println("next");
             activity.setText(StartWorkoutActivity.workoutExercises.get(curInd));
             ((TextView) findViewById(R.id.timeRemaining)).setText("seconds remaining");
-            new ActivityCountDownTimer(StartWorkoutActivity.exerciseSeconds.get(curInd) * 1000, 1000).start();
+            timer = new ActivityCountDownTimer(StartWorkoutActivity.exerciseSeconds.get(curInd) * 1000, 1000);
+            timer.start();
+            v.vibrate(2000);
         }
     }
 
+    static void quit() {
+        quit = true;
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            onPause();
+            quit();
+            QuitDialog qd = new QuitDialog();
+            qd.onCreateDialog(this.bundle);
+        }
+        return super.onKeyDown(keyCode, event);
+    }
 }
 
