37f37d06bbd2d3880d81903c7a0079649cec0737
Trang <Trang>
2016-10-09 04:15:17 -0400

    Merge branch 'master' of https://github.com/l-tn3314/FinesseFitness
    # Conflicts:
    #	app/src/main/java/com/example/finessefitness/MainActivity.java
    #	app/src/main/java/hard_code/Workouts.java
    #	build/intermediates/dex-cache/cache.xml

--- app/src/main/java/com/example/finessefitness/MainActivity.java
+++ app/src/main/java/com/example/finessefitness/MainActivity.java
@@ -116,7 +116,7 @@ public class MainActivity extends AppCompatActivity implements
     @Override
     public void onLoggedIn() {
         Log.d("MainActivity", "User logged in");
-        //mPlayer.playUri(null, "spotify:track:2TpxZ7JUBn3uw46aR7qd6V", 0, 0);
+        mPlayer.playUri(null, "spotify:track:2TpxZ7JUBn3uw46aR7qd6V", 0, 0);
     }
 
     @Override
