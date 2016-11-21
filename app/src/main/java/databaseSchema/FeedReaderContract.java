f1dceeba3c91bebc378eff6b15aa3efa248be7d3
ssai123 <sbs.sai@gmail.com>
2016-11-20 22:32:38 -0500

    updated schema and login

---
+++ app/src/main/java/databaseSchema/FeedReaderContract.java
@@ -0,0 +1,19 @@
+package databaseSchema;
+
+import android.provider.BaseColumns;
+
+/** This contract class is a container for constants that
+ * define names for URIs, tables, and columns */
+public final class FeedReaderContract {
+    // To prevent someone from accidentally instantiating the contract class,
+    // make the constructor private.
+    private FeedReaderContract() {}
+
+    /* Inner class that defines the table contents */
+    public static class FeedEntry implements BaseColumns {
+        public static final String TABLE_NAME = "entry";
+        public static final String COLUMN_NAME_TITLE = "title";
+        public static final String COLUMN_NAME_SUBTITLE = "subtitle";
+    }
+}
+
