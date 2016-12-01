3bff67fac2c514a6a8ec1d1cc7d2be2ef6a8a0f7
ssai123 <sbs.sai@gmail.com>
2016-11-30 23:34:56 -0500

    Added returning member page

---
+++ app/src/main/java/com/example/finessefitness/ReturnActivity.java
@@ -0,0 +1,19 @@
+package com.example.finessefitness;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class ReturnActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_returning);
+
+    }
+
+    public void Login(View view) {
+        // Need to check whether the username is in the database and
+        // if the password matches
+    }
+}
